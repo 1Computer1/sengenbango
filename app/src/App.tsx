7915023c7073684a16fe791b62bd6053ab23d4e8
1Computer <onecomputer00@gmail.com>
2023-10-11 19:56:30 -0400

    Add confirmation for when search is done

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Search } from './components/Search';
-import { parseQuery } from './query/parser';
+import { Result, parseQuery } from './query/parser';
 import { DefaultSettings, ParallelDocument, Settings, queryDocuments } from './query/api';
 import { SearchResult } from './components/SearchResult';
 import { SearchSettings } from './components/SearchSettings';
@@ -9,56 +9,70 @@ import { LanguageSwitch } from './components/LanguageSwitch';
 
 function App() {
 	const [settings, setSettings] = useState<Settings>(DefaultSettings);
-	const [results, setResults] = useState<ParallelDocument[] | null>(null);
-	const [errors, setErrors] = useState<string | null>(null);
+	const [results, setResults] = useState<Result<ParallelDocument[], string> | null>(null);
+	const [isChanged, setIsChanged] = useState(false);
 
 	return (
 		<div className="container mx-auto p-4">
 			<div className="flex flex-col items-center gap-3">
 				<Search
 					className="w-full lg:w-1/2"
+					searchDone={results != null && !isChanged}
+					onChange={() => setIsChanged(true)}
 					onSubmit={async (text) => {
+						if (!text) {
+							return;
+						}
 						const q = parseQuery(text);
 						if (q.ok) {
 							const r = await queryDocuments(q.value, settings);
 							if (r.ok) {
-								setResults(r.value);
-								setErrors(null);
+								setResults(r);
 							} else {
-								setResults(null);
-								setErrors(String(r.error));
+								setResults({ ok: false, error: String(r.error) });
 							}
 						} else {
-							setResults(null);
-							setErrors(q.error.join('\n'));
+							setResults({ ok: false, error: q.error.join('\n') });
 						}
+						setIsChanged(false);
 					}}
 				/>
 				<div className="flex flex-row items-center gap-4">
-					<SearchSettings value={settings} onChange={setSettings} />
+					<SearchSettings
+						value={settings}
+						onChange={(t) => {
+							setSettings(t);
+							setIsChanged(true);
+						}}
+					/>
 					<LanguageSwitch
 						value={settings.lang}
-						onChange={(lang) =>
+						onChange={(lang) => {
 							setSettings(
 								produce((d) => {
 									d.lang = lang;
 								}),
-							)
-						}
+							);
+							setIsChanged(true);
+						}}
 					/>
 				</div>
-				{results && results.length ? (
-					<ol className="flex flex-col gap-1.5 w-full lg:w-2/3">
-						{results.map((pdoc) => (
-							<li key={pdoc.en + ' ' + pdoc.jp}>
-								<SearchResult pdoc={pdoc} />
-							</li>
-						))}
-					</ol>
-				) : errors ? (
-					<div className="text-red-600">{errors}</div>
+				{results && results.ok ? (
+					results.value.length ? (
+						<ol className="flex flex-col gap-1.5 w-full lg:w-2/3">
+							{results.value.map((pdoc) => (
+								<li key={pdoc.en + ' ' + pdoc.jp}>
+									<SearchResult pdoc={pdoc} />
+								</li>
+							))}
+						</ol>
+					) : (
+						<div>No results found.</div>
+					)
+				) : results && !results.ok ? (
+					<div className="text-red-600">{results.error}</div>
 				) : (
-					<div>No results found.</div>
+					<div>Type something to search!</div>
 				)}
 			</div>
 		</div>
--- app/src/components/Search.tsx
+++ app/src/components/Search.tsx
@@ -1,17 +1,24 @@
 import { Button, Input, SearchField, SearchFieldProps } from 'react-aria-components';
-import { FaX } from 'react-icons/fa6';
+import { FaCheck, FaXmark } from 'react-icons/fa6';
 
-interface SearchBarProps extends SearchFieldProps {}
+interface SearchBarProps extends SearchFieldProps {
+	searchDone: boolean;
+}
 
-export function Search({ ...props }: SearchBarProps) {
+export function Search({ searchDone, ...props }: SearchBarProps) {
 	return (
 		<SearchField aria-label="Search" {...props}>
 			{({ isEmpty }) => (
-				<div className="grid grid-areas-[input_clear_submit_help] grid-cols-[1fr,auto]">
-					<Input className="grid-in-[input] border border-gray-300 rounded-md p-2 pr-10 lg:text-2xl" />
+				<div className="grid grid-areas-[input_done_clear] grid-cols-[1fr,auto,auto] items-center">
+					<Input className="grid-in-[input] border border-gray-300 rounded-md p-2 pr-20 lg:text-2xl" />
+					{searchDone && (
+						<div className="grid-in-[done] rounded-full -ml-16 z-10 text-green-600">
+							<FaCheck />
+						</div>
+					)}
 					{!isEmpty && (
-						<Button className="grid-in-[clear] rounded-full -ml-8">
-							<FaX />
+						<Button className="grid-in-[clear] rounded-full -ml-8 z-10">
+							<FaXmark />
 						</Button>
 					)}
 				</div>
