e2cd6bd37a4dd2e31fd8e9224d9251a735a6933b
1Computer <onecomputer00@gmail.com>
2023-10-23 22:25:07 -0400

    Add copy to clipboard button

--- app/src/components/SearchResult.tsx
+++ app/src/components/SearchResult.tsx
@@ -1,6 +1,6 @@
-import { Text } from 'react-aria-components';
+import { Button, Text } from 'react-aria-components';
 import { ParallelDocument } from '../query/api';
-import { FaStar } from 'react-icons/fa6';
+import { FaClipboardCheck, FaRegClipboard, FaStar } from 'react-icons/fa6';
 import { useEffect, useState } from 'react';
 import clsx from 'clsx';
 
@@ -11,35 +11,55 @@ export interface SearchResultProps {
 
 export function SearchResult({ pdoc: { source, jp, en, score }, showEnglish }: SearchResultProps) {
 	const [isShowingEnglish, setIsShowingEnglish] = useState(showEnglish);
+	const [isCopied, setIsCopied] = useState(false);
 
 	useEffect(() => {
 		setIsShowingEnglish(showEnglish);
 	}, [showEnglish]);
 
 	return (
-		<div className="flex flex-col border border-gray-300 dark:border-gray-600 rounded-md p-2 gap-0.5">
-			<Text>{jp}</Text>
-			<Text
-				className={clsx(
-					'text-sm w-fit rounded-md transition',
-					!isShowingEnglish &&
-						'text-zinc-300 bg-zinc-300 dark:text-zinc-700 dark:bg-zinc-700 cursor-pointer select-none',
-					isShowingEnglish && 'cursor-text select-text',
-				)}
-				onClick={() => {
-					setIsShowingEnglish(true);
+		<div className="grid grid-areas-[text_copy,source_star] grid-cols-[1fr,auto] grid-rows-[1fr,auto] border border-gray-300 dark:border-gray-600 rounded-md p-2 gap-0.5">
+			<div className="grid-in-[text] flex flex-col gap-0.5">
+				<Text className="w-fit">{jp}</Text>
+				<Text
+					className={clsx(
+						'text-sm w-fit rounded-md transition',
+						!isShowingEnglish &&
+							'text-zinc-300 bg-zinc-300 dark:text-zinc-700 dark:bg-zinc-700 cursor-pointer select-none',
+						isShowingEnglish && 'cursor-text select-text',
+					)}
+					onClick={() => {
+						setIsShowingEnglish(true);
+					}}
+				>
+					{en}
+				</Text>
+			</div>
+			<Button
+				aria-label="Copy to Clipboard"
+				className="grid-in-[copy] self-start justify-self-end relative text-gray-400 w-4 h-4"
+				onPress={() => {
+					window.navigator.clipboard.writeText(jp + '\n' + en);
+					setIsCopied(true);
+					setTimeout(() => setIsCopied(false), 1000);
 				}}
 			>
-				{en}
-			</Text>
-			<div className="flex flex-row justify-between items-end">
+				<div className="absolute top-0 left-0">
+					<FaRegClipboard />
+				</div>
+				<div
+					className={clsx(
+						'absolute top-0 left-0 transition-opacity ease-in-out',
+						isCopied ? 'opacity-100 duration-0' : 'opacity-0 duration-300',
+					)}
+				>
+					<FaClipboardCheck />
+				</div>
+			</Button>
+			<div className="grid-in-[source]">
 				<Text className="text-xs text-gray-400">{source}</Text>
-				{score >= -100 && (
-					<span className="text-xs text-yellow-500">
-						<FaStar />
-					</span>
-				)}
 			</div>
+			<span className="grid-in-[star] place-self-end text-xs text-yellow-500">{score >= -100 && <FaStar />}</span>
 		</div>
 	);
 }
